a7dfe3afdbae34becc07e974acc2ae45db81c2f7
Angel Madin <50140073+achkataa@users.noreply.github.com>
2021-04-04 11:40:12 +0300

    Delete RegEx directory

--- RegEx/1.Match Full Name.py
+++
@@ -1,8 +0,0 @@
-import re
-text = input()
-
-validation = r"\b[A-Z][a-z]+\s[A-Z][a-z]+\b"
-
-names = re.findall(validation, text)
-
-print(" ".join(names))
--- RegEx/2.Find Variable Names in Sentences.py
+++
@@ -1,9 +0,0 @@
-import re
-
-line = input()
-
-string = r"(?<=\b_)[A-Za-z0-9]+\b"
-
-validation = re.findall(string, line)
-
-print(",".join(validation))
